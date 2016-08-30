2f08bb054b31983ed65c5ead0bd9ab1d65ddc379
Clemens Hemmerling <clemenshemmerling@gmail.com>
2016-08-30 15:02:54 -0600

    Separamos todos los contenedores

---
+++ owncloud/config/config.php
@@ -0,0 +1,21 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc5witnwvttl',
+  'passwordsalt' => 'FW3cEgB+RDhd5p7kGVAtkeQ1ofjJ8S',
+  'secret' => 'JYO1ybJseY35zc6EmihACXjFbAeMuJEpmQPy/Mpdl3uyljJb',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost:8080',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'overwrite.cli.url' => 'http://localhost:8080',
+  'dbtype' => 'mysql',
+  'version' => '9.1.0.15',
+  'dbname' => 'owncloud',
+  'dbhost' => 'mysql',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'user',
+  'dbpassword' => 'userpass',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+);
